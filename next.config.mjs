ff0a4b39c00722df93fad37a39fb3fe07e02f19f
Ahmed Raza <101836398+devdazzlee@users.noreply.github.com>
2025-06-14 18:35:19 +0500

    Add Code AR

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',         // ← generate a static export
-  images: { unoptimized: true }  // ← skip the Next.js image optimizer
+  output: 'export',
 }
 
 export default nextConfig
